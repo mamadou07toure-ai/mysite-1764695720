1998c4d321ad2fcc0a8a06a5dc4955cb0c47148d
mamadou07toure-ai <mamadou07toure@gmail.com>
2025-12-02 17:15:28 +0000

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Portfolio from './pages/Portfolio'
+import Team from './pages/Team'
+import Process from './pages/Process'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import ScrollToTop from './components/ScrollToTop'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/process" element={<Process />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+      <ScrollToTop />
+    </div>
+  )
+}
+
+export default App
